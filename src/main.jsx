73f5aebd8bd1058554ab8007288c6527af73946d
Samriddhi Tripathi <SamriddhiT@bpl.netlink.com>
2025-07-13 02:56:51 -0700

    Switch to HashRouter for GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import { Toaster } from "react-hot-toast";
@@ -10,10 +10,10 @@ import { Toaster } from "react-hot-toast";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
         <Toaster position="top-center" toastOptions={{ duration: 2000 }} /> {/* ✅ Toasts */}
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
